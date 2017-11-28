0b2767d3fe14ce793a906f0e0c1bc3fdc3c64802
Rapando Sam <samsonrapando@gmail.com>
2017-11-28 23:58:42 +0300

    added configurations

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+This file will contain any global configurations
+'''
+
+brighter_monday = "https://www.brightermonday.co.ke/jobs/"
+
+headers = {
+    'content-type': 'application/json',
+    'cache-control': "no-cache",
+}
